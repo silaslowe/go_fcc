65314ca5f014d4d64b39da9ecd45c2316b87ed69
silaslowe@gmail.com <silaslowe@gmail.com>
2021-08-10 11:45:28 -0500

    added tags and embedding

--- src/silaslowe/firstapp/Structs.go
+++ src/silaslowe/firstapp/Structs.go
@@ -1,34 +1,32 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Doctor struct {
-	Number int
-	ActorName string
-	Companions []string
-}
+// type Doctor struct {
+// 	Number int
+// 	ActorName string
+// 	Companions []string
+// }
 
-func main() {
-	aDoctor := Doctor {
-		Number: 3,
-		ActorName: "Jon Pertwee",
-		Companions: []string {
-			"Liz Shaw",
-			"Jo Grant",
-			"Sarah Jane Smith",
-		},
-	}
-	fmt.Println(aDoctor)
-	fmt.Println(aDoctor.ActorName)
-	fmt.Println(aDoctor.Companions[1])
+// func main() {
+// 	aDoctor := Doctor {
+// 		Number: 3,
+// 		ActorName: "Jon Pertwee",
+// 		Companions: []string {
+// 			"Liz Shaw",
+// 			"Jo Grant",
+// 			"Sarah Jane Smith",
+// 		},
+// 	}
+// 	fmt.Println(aDoctor)
+// 	fmt.Println(aDoctor.ActorName)
+// 	fmt.Println(aDoctor.Companions[1])
 
-	// Anonomous struct
+// 	// Anonomous struct
 
-	bDoctor := struct{name string}{name: "Silas Lowe"}
-	anotherDoctor := bDoctor
-	anotherDoctor.name = "Tom Baker"
-	fmt.Println(bDoctor)
-	fmt.Println(anotherDoctor)
-
-
-}
+// 	bDoctor := struct{name string}{name: "Silas Lowe"}
+// 	anotherDoctor := bDoctor
+// 	anotherDoctor.name = "Tom Baker"
+// 	fmt.Println(bDoctor)
+// 	fmt.Println(anotherDoctor)
+// }
