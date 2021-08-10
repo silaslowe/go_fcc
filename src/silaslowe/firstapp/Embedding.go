7e88eb01161c9344bac2aeaa1e8e99d56a3e5d12
silaslowe@gmail.com <silaslowe@gmail.com>
2021-08-10 15:06:00 -0500

    ifs dun

--- src/silaslowe/firstapp/Embedding.go
+++ src/silaslowe/firstapp/Embedding.go
@@ -1,36 +1,36 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"reflect"
-)	
+// import (
+// 	"fmt"
+// 	"reflect"
+// )	
 
-type Animal struct {
-	Name string
-	Origin string
-}
+// type Animal struct {
+// 	Name string
+// 	Origin string
+// }
 
-type Bird struct {
-	Animal
-	SpeedKPH float32
-	CanFly bool
-}
+// type Bird struct {
+// 	Animal
+// 	SpeedKPH float32
+// 	CanFly bool
+// }
 
-type Aminal struct {
-	Name string `required max: "100"`
-	Origin string
-}
+// type Aminal struct {
+// 	Name string `required max: "100"`
+// 	Origin string
+// }
 
-func main() {
-	b := Bird{}
-	b.Name = "Emu"
-	b.Origin = "Australia"
-	b.SpeedKPH = 48
-	b.CanFly = false
-	fmt.Println(b)
-	fmt.Println(b.Origin)
+// func main() {
+// 	b := Bird{}
+// 	b.Name = "Emu"
+// 	b.Origin = "Australia"
+// 	b.SpeedKPH = 48
+// 	b.CanFly = false
+// 	fmt.Println(b)
+// 	fmt.Println(b.Origin)
 
-	t := reflect.TypeOf(Aminal{})
-	field, _ := t.FieldByName("Name")
-	fmt.Println(field.Tag)
-}
+// 	t := reflect.TypeOf(Aminal{})
+// 	field, _ := t.FieldByName("Name")
+// 	fmt.Println(field.Tag)
+// }
