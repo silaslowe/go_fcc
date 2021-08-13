b65527319d8101a481582fe3cdb4044414f81b10
silaslowe@gmail.com <silaslowe@gmail.com>
2021-08-13 15:16:25 -0500

    interface with multiple methods. Interface inside of interface

---
+++ src/silaslowe/firstapp/Interfaces.go
@@ -0,0 +1,100 @@
+package main
+
+func main() {
+
+// // I'm not really sure what is happening here
+
+// 	var w Writer = ConsoleWriter{}
+// 	w.Write([]byte("Hello Werld"))
+// }
+
+// type Writer interface {
+// 		Write([]byte) (int, error) 
+// }
+
+// type ConsoleWriter struct {}
+
+// func (cw ConsoleWriter) Write(data []byte) (int, error) {
+// 	n, err := fmt.Println(string(data))
+// 	return n, err
+
+// 	myInt := IntCounter(0)
+// 	var inc Incrementer = &myInt
+// 	for i:= 0; i < 10; i++ {
+// 		fmt.Println(inc.Increment())
+// 	}
+// }
+
+// type Incrementer interface {
+// 	Increment() int
+// }
+
+// type IntCounter int
+
+// func (ic *IntCounter) Increment() int {
+// 	*ic++
+// 	return int(*ic)
+
+// ****************************************
+
+// // Interface with mutliple methods
+
+// 	var wc WriterCloser = NewBufferedWriterCloser()
+// 	wc.Write([]byte("Hello Youtube listeners, this is a test"))
+// 	wc.Close()
+// }
+
+// type Writer interface {
+// 	Write([]byte) (int, error)
+// }
+
+// type Closer interface {
+// 	Close() error
+// }
+
+// type WriterCloser interface{
+// 	Writer
+// 	Closer
+// }
+
+// type BufferedWriterCloser struct {
+// 	buffer *bytes.Buffer
+// }
+
+// func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+// 	n, err := bwc.buffer.Write(data)
+// 	if err != nil {
+// 		return 0, err 
+// 	}
+
+// 	v := make([]byte, 8)
+// 	for bwc.buffer.Len() > 8 {
+// 		_, err := bwc.buffer.Read(v)
+// 		if err != nil {
+// 			return 0, err
+// 		}
+// 		_, err = fmt.Println(string(v))
+// 		if err != nil {
+// 			return 0, err
+// 		}
+// 	}
+// 	return n, nil
+// }
+
+// func (bwc *BufferedWriterCloser) Close() error {
+// 	for bwc.buffer.Len() > 0 {
+// 		data := bwc.buffer.Next(8)
+// 		_, err := fmt.Println(string(data))
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
+
+
+// func NewBufferedWriterCloser() *BufferedWriterCloser {
+// 	return &BufferedWriterCloser{
+// 		buffer: bytes.NewBuffer([]byte{}),
+// 	}
+}
